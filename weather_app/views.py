53b28c25c756b7ae19a97eb9f68355b573651629
Elizabeth Taylor <elizabethtaylor@Elizabeths-MacBook-Pro-2.local>
2023-11-09 13:40:44 -0500

    .env test

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,22 +1,26 @@
 import datetime
-
+from dotenv import load_dotenv
 import requests
 from django.shortcuts import render
+import os
 
 # Create your views here.
+def configure():
+    load_dotenv()
+    
 def index(request):
-    API_KEY = "<YOUR API KEY>"
-    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
+    configure()
+    current_weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={{}}&appid={os.getenv('api_key')}"
+    forecast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={{}}&lon={{}}&exclude=current,minutely,hourly,alerts&appid={os.getenv('api_key')}"
 
     if request.method == "POST":
         city1 = request.POST['city1']
         city2 = request.POST.get('city2', None)
 
-        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
+        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, os.getenv('api_key'), current_weather_url, forecast_url)
 
         if city2:
-            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_url)
+            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, os.getenv('api_key'), current_weather_url, forecast_url)
         else:
             weather_data2, daily_forecasts2 = None, None
 
@@ -31,10 +35,11 @@ def index(request):
         return render(request, "weather_app/index.html")
 
 def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url): 
+    configure()
     response = requests.get(current_weather_url.format(city, api_key)).json()
-    lat, lon = response.get('coord', ['lat']), response.get('coord', ['lon'])
+    lat, lon = response.get('coord', {}).get('lat'), response.get('coord', {}).get('lon')
     forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
-
+    print("!!",response)
     weather_data = {
         "city": city,
         "temperature": round(response['main']['temp'] - 273.15, 2),
